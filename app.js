6e9425becb6c0c8e84775fedb7a497fce96c1ecf
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-07-13 19:24:45 -0700

    about page updated 7.13

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ function openNav() {
 }
 
 function closeNav() {
-    document.getElementById('myNav').style.height = '0%';
+  document.getElementById('myNav').style.height = '0%';
 }
 
 function openNav() {
