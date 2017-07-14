764e9a4a5ca4adae443b4803ecdc32443f760a62
Rowen Scout <rowenscout@gmail.com>
2017-07-13 19:16:05 -0700

    Added event listeners.

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function checkWin(x) {
 for (var i = 0; i < 9; i++) {
   var imgIds = [];
   imgIds[i] = document.getElementById(i);
-//  imgIds[i].addEventListener('click', handleTurn);
+  imgIds[i].addEventListener('click', handleTurn);
 }
 
 function addUp() {
